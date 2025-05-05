aee206f71c509b03c50c499ab086e4fce4c6f378
3MR-Karim <amrkarim000@gmail.com>
2025-05-05 23:08:03 +0300

    ProductsController

---
+++ Controllers/BaseApiControlller.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Talabat.Solution.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseApiControlller : ControllerBase
+    { // if RouteAPi  Repeat for remove
+      // [Route("api/[controller]")]
+       //[ApiController]
+    }
+}
---
+++ Controllers/ProductController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Talabat.Solution.Controllers
+{
+    public class ProductController : BaseApiControlller /*IDEa becuase seoned inherit from base the routing becaome gerenrik */
+    {
+        // dont have obj from call repository how build in gereiric 
+
+      //  public ProductController(IGenericrposi)
+      //  {
+            
+      //  }
+
+      //  // build EndPoint 
+
+      //  [HttpGet]
+      //public List<Product>  getAllProeuct()
+      //  {
+
+
+
+      //  }
+
+
+
+
+
+
+
+
+    }
+}
+
+
+// dfifference baseapi conroller VS CommonController 
+// baseAPIcontroller  base for all api controoller
+// commonConotreoller have common endPoint every module use it  endpoint use everyModule
