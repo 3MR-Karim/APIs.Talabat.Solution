6141554ab34aaa72714a617857aae1b435ce6a5e
3MR-Karim <amrkarim000@gmail.com>
2025-05-06 19:20:05 +0300

    DbContext, ProductConfigurations, Migrations

--- Program.cs
+++ Program.cs
@@ -1,12 +1,17 @@
- 
+
+using Microsoft.EntityFrameworkCore;
+using Talabat.Repository.Data;
+
 namespace Talabat.Solution
 {
     public class Program
     {
         public static void Main(string[] args)
         {
+         
             var builder = WebApplication.CreateBuilder(args);
 
+            
             // Add services to the container.
 
             builder.Services.AddControllers();
@@ -14,7 +19,12 @@ namespace Talabat.Solution
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
+            builder.Services.AddDbContext<StoreContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
             var app = builder.Build();
+           
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
