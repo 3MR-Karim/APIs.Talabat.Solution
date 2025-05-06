9b62e2b7fd3ed2d130a5928eb3c729a66c481bc2
3MR-Karim <amrkarim000@gmail.com>
2025-05-06 20:06:41 +0300

    Update DB and How Do DI Explict

--- Program.cs
+++ Program.cs
@@ -1,12 +1,13 @@
 
 using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 using Talabat.Repository.Data;
 
 namespace Talabat.Solution
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task  Main(string[] args)
         {
          
             var builder = WebApplication.CreateBuilder(args);
@@ -24,7 +25,25 @@ namespace Talabat.Solution
                 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
             });
             var app = builder.Build();
-           
+
+            #region How Clr create opject not implicit DI but Explicit  this beccause need migration then do look Database to apply migration
+            using var scoperd = app.Services.CreateScope();
+             var Services = scoperd.ServiceProvider;
+            var _dbContext= Services.GetRequiredService<StoreContext>();
+         var loggerFactory= Services.GetRequiredService<ILoggerFactory>();
+            try
+            {
+            await _dbContext.Database.MigrateAsync();  // update db becuse mifate everseconeed contintu if run app found migration not apply DLR DO Apply
+                // and use because data Seeding and if deploy API and cant open PMA for applymigraotn becuase in server  i talk becuase work 
+
+            }
+            catch (Exception ex)
+            {
+                var logger = loggerFactory.CreateLogger<Program>();
+                logger.LogError(ex,"an error has been occured during apply the migration");
+            }
+            #endregion
+
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
